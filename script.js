a92fb5c7ba675ded0a06d76ed0319d57a85a1b35
Amir <109604111+AmirH32@users.noreply.github.com>
2022-08-24 22:28:37 +0100

    Update script.js
    Adds a function (addToList) for the button press, allowing for new list elements to be added to the unordered list. 
    Also, turns the addEventListener loop from the previous patch and the getElements process into functions so that they can be called repeatedly allowing for the javascript to add EventListeners to any new list elements and their children.

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
- let checklist = document.getElementById("checklist");
-// checklist is the whole <ul> container with the id ("checklist")
-let items = checklist.querySelectorAll("li");
-// items is a list of all the <li> elements
-let inputs = checklist.querySelectorAll("input");
-// inputs is a list of all the input elements
+addListeners(getElements());
+// adds event listeners to every <li> and its child <input> so that it can be clicked on and edited. It gathers every element from the getElements() function.
 
-for (let i = 0; i< items.length;i++){
-    items[i].addEventListener("click", editItem);
-    // Each item listens for a click and if it is clicked, the editItem function is run
-    inputs[i].addEventListener("blur", updateItem)
-    // Each input listens for a blur and if so it runs the updateItem function
-    inputs[i].addEventListener("keypress", itemKeypress)
-    // Each input listens for any keypress
+function getElements(){
+    let checklist = document.getElementById("checklist");
+    // checklist is the whole <ul> container with the id ("checklist")
+    let items = checklist.querySelectorAll("li");
+    // items is a list of all the <li> elements
+    let inputs = checklist.querySelectorAll("input");
+    // inputs is a list of all the input elements
+    return [checklist,items,inputs];
+}
+
+function addListeners(list){
+    let checklist = list[0]
+    let items = list[1]
+    let inputs = list[2]
+    for (let i = 0; i< items.length;i++){
+        items[i].addEventListener("click", editItem);
+        // Each item listens for a click and if it is clicked, the editItem function is run
+        inputs[i].addEventListener("blur", updateItem);
+        // Each input listens for a blur and if so it runs the updateItem function
+        inputs[i].addEventListener("keypress", itemKeypress);
+        // Each input listens for any keypress
+    }
 }
 
 function editItem(){
@@ -24,6 +35,7 @@ function editItem(){
     input.setSelectionRange(0, input.value.length);
     // selects the whole text within the edit element
 }
+
 function updateItem(){
     this.previousElementSibling.innerHTML = this.value;
     // the sibling <span> elements value is changed to the edit's value.
@@ -34,9 +46,22 @@ function updateItem(){
 function itemKeypress(event){
     if (event.which === 13){
         // checks if the keypressed is enter (13)
-        updateItem.call(this)
+        updateItem.call(this);
         // passes the element you are on from keypress to updateItem
     }else{
-        return
+        return;
     }
+}
+
+function addToList(){
+    const newItem = document.createElement('li');
+    checklist.append(newItem);
+    const span = document.createElement('span');
+    newItem.append(span);
+    const input = document.createElement('input');
+    input.value = "Enter text here:";
+    newItem.append(input);
+    // creates a new <li> element which has the children: <input> and <span>. This <li> element becomes the child of the parent element: <ul id="checklist">
+    addListeners(getElements())
+    // Runs the addListeners function to add listeners to the new <li> and its <input>.
 }
